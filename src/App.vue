686ea9359dfcd4abe4b2a12599607b9755155733
Tommaso <tommaso.lodesani@gmail.com>
2022-06-10 14:59:26 +0200

    setting project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DiscList/>
+    
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import DiscList from './components/DiscList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DiscList
   }
 }
 </script>
